c1502eadf09ee7b350f705026e6c2cc5db813a75
Marcos Costa <marcoscosta@air-de-marcos.lan>
2024-05-01 19:14:32 +0100

    upload app completed

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    
+    var secondsRemanining = 60
 
-
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle!
+        
+        secondsRemanining = eggTimes[hardness]!
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target:self, selector:
+            #selector(updateTimer), userInfo:nil, repeats: true)
+        
+    }
+    @objc func updateTimer() {
+        if secondsRemanining > 0 {
+            print("\(secondsRemanining) seconds.")
+            secondsRemanining -= 1
+        }
+    }
+    
 }
+
