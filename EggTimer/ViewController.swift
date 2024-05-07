d4294ac52a602917877bbc567c29ae8859558474
Marcos Costa <marcoscosta@air-de-marcos.lan>
2024-05-07 09:45:20 +0100

    app uploaded

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,28 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-    
-    var eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
-    
-    var secondsRemanining = 60
 
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var tittleLabel: UILabel!
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var secondsRemanining = 60
+    var timer = Timer()
+    var player: AVAudioPlayer!
+    var totalTime = 0
+    var secondsPassed = 0
+
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        
+    
+
+        timer.invalidate()
         let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
         
-        secondsRemanining = eggTimes[hardness]!
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        tittleLabel.text = hardness
         
-        Timer.scheduledTimer(timeInterval: 1.0, target:self, selector:
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target:self, selector:
             #selector(updateTimer), userInfo:nil, repeats: true)
         
     }
     @objc func updateTimer() {
-        if secondsRemanining > 0 {
-            print("\(secondsRemanining) seconds.")
-            secondsRemanining -= 1
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            print(Float(secondsPassed) / Float(totalTime))
+        } else {
+            timer.invalidate()
+            tittleLabel.text = "DONE!"
+            
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+            
         }
     }
     
